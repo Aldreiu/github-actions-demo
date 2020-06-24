10b92a78fe2b1328414b47795beb352653a8916e
Aldreiu <47169934+Aldreiu@users.noreply.github.com>
2020-06-24 05:28:37 +0000

    Deploying to gh-pages from  @ 7cda8fa91cf15419e6421bf733873c577bb6e2c2 🚀

---
+++ precache-manifest.2bf7a019d8194dcd680ea8c83f2e15bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06900e24c525b20d5e5f5dabec9ac695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b660d509b2d9a22c2f5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "802aac82f49d87fbd057",
+    "url": "/static/js/2.2d4cfb36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d4cfb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b660d509b2d9a22c2f5",
+    "url": "/static/js/main.8c7a1cf9.chunk.js"
+  },
+  {
+    "revision": "649f4545b63f5767a5b2",
+    "url": "/static/js/runtime-main.89230110.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
